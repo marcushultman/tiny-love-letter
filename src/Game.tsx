5215d45d778810ae6850ccf381a0ad0182da8248
Marcus Hultman <marcus.hultman1@gmail.com>
2021-09-03 19:11:15 +0200

    add missing Game

---
+++ src/Game.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Game.scss';
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className= "Game" >
+        Hej fan
+      </div>
+    );
+  }
+}
