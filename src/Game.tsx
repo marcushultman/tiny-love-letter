6f18b79ce3a214350109dc0d04590489162b9062
Marcus Hultman <marcus.hultman1@gmail.com>
2021-09-03 21:29:32 +0200

    mock state

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,8 +1,11 @@
 import { Component } from 'react';
 import './Game.scss';
 import { State } from './schema'
-import Card from './Card'
 import { readState } from './firebase';
+
+import Card from './Card'
+import EmptyCard from './EmptyCard'
+import MultiCard from './MultiCard'
 
 interface GameProps {
   me: string;
@@ -41,21 +44,52 @@ export default class Game extends Component<GameProps, GameState> {
 
   async detectChange() {
 
-    const state = await readState(this.props.token);
-    this.setState({ loading: false, state });
+    // mock
+    if (this.state.loading) {
+      setTimeout(() => {
+        const state = {
+          seed: 42,
+          players: ['abc', 'def'],
+          actions: [
+            { index: 0, meta: {} },
+            { index: 1, meta: {} },
+          ],
+        }
+        this.setState({ loading: false, state });
+      }, 500);
+    }
 
-    console.log({ state });
+    // const state = await readState(this.props.token);
+    // this.setState({ loading: false, state });
+  }
+
+  playedCards(i: number) {
+    // const actions = this.state.state.actions.slice(i).filter((_, i) => i % 2 === 0);
+
+    // return (<div>{ JSON.stringify({ a: this.state.state.actions, b: actions}, null, 2) }</div>);
+
+    // <MultiCard cards={ this.state.state.actions.map(() => 0) } />
+    return <EmptyCard />
   }
 
   render() {
+    if (this.state.loading) {
+      return (<div className= "Game" >Loading...</div>);
+    }
+
+    const { actions, players } = this.state.state;
 
     let myHand;
-    console.log(this.state);
-    if (!this.state.loading && this.state.state.actions && this.state.state.actions.length % this.state.state.players.indexOf(this.props.me) === 0) {
+    if (actions.length % players.indexOf(this.props.me) === 0) {
       myHand =
-      <div className="Game-myhand">
-        <Card />
-        <Card />
+      <div>
+        <h3>
+          <b>Your turn, play a card:</b>
+        </h3>
+        <div className="Game-myhand">
+          <Card onSelect={ () => {} } />
+          <Card onSelect={ () => {} } />
+        </div>
       </div>;
     } else if(!this.state.loading) {
       myHand =
@@ -66,8 +100,16 @@ export default class Game extends Component<GameProps, GameState> {
 
     return (
       <div className= "Game" >
+        <div>#Players: { players.length }</div>
         <div className="Game-vask"><Card /> <Card /> <Card /> <Card /></div>
-        <div className="Game-played">Waiting...</div>
+        <div className="Game-played">{
+          players.map((player, i) => (
+            <div>
+              <p>{ player === this.props.me ? <b>{ player }</b> : player }</p>
+              { this.playedCards(i) }
+            </div>
+          ))
+        }</div>
         { myHand }
       </div>
     );
